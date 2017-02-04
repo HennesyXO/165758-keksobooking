eb29382ec84f03b5e45c8dfdcdaf4185e8a603cd
Irina <irstrashuk@gmail.com>
2017-02-04 10:33:24 +0200

    Some function improvement

--- js/form.js
+++ js/form.js
@@ -2,16 +2,15 @@
 // 1. Показ карточки объявления
 var pin = document.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
-if (dialog) {
-  var dialogClose = dialog.querySelector('.dialog__close');
-}
+var dialogClose = dialog.querySelector('.dialog__close');
+
 // удаляем оранжевый ободок у существующих классов
-var removeActivePin = function () {
+function removeActivePin() {
   var actPin = document.querySelector('.pin--active');
   if (actPin) {
     actPin.classList.remove('pin--active');
   }
-};
+}
 
 if (pin) {
   Array.prototype.forEach.call(pin, function (singlePin) {
@@ -23,24 +22,24 @@ if (pin) {
   });
 }
 // 2. Закрытие карточки объявления
-if (dialogClose) {
-  dialogClose.addEventListener('click', function () {
-    removeActivePin();
-    dialog.style.display = 'none';
-  });
+function closeModal() {
+  removeActivePin();
+  dialog.style.display = 'none';
 }
+
+dialogClose.addEventListener('click', closeModal);
 
 // 3. Проверка правильности введенных данных
 // Заголовок объявления
 var noticeTitle = document.querySelector('#title');
 noticeTitle.required = true;
-noticeTitle.min = 30;
-noticeTitle.max = 100;
+noticeTitle.minLength = 30;
+noticeTitle.maxLength = 100;
 // Цена за ночь
 var noticePrice = document.querySelector('#price');
 noticePrice.required = true;
-noticePrice.min = 1000;
-noticePrice.max = 1000000;
+noticePrice.minLength = 1000;
+noticePrice.maxLength = 1000000;
 
 // Адрес
 var noticeAddress = document.querySelector('#address');
@@ -50,42 +49,49 @@ noticeAddress.required = true;
 var timeIn = document.querySelector('#time');
 var timeOut = document.querySelector('#timeout');
 
-if (timeIn && timeOut) {
-  timeIn.addEventListener('change', function () {
-    timeOut.value = timeIn.value;
-  });
-
-  timeOut.addEventListener('change', function () {
-    timeIn.value = timeOut.value;
-  });
+function connectOptions() {
+  timeOut.value = timeIn.value;
 }
 
+function connectOptionsBack() {
+  timeIn.value = timeOut.value;
+}
+
+if (timeIn && timeOut) {
+  timeIn.addEventListener('change', connectOptions);
+  timeOut.addEventListener('change', connectOptionsBack);
+}
 
 //  «Тип жилья» синхронизировано с минимальной ценой
 var apartType = document.querySelector('#type');
 
+function switchPrice() {
+  switch (apartType.value) {
+    case 'appartment': noticePrice.min = 1000;
+      break;
+    case 'hovel': noticePrice.min = 0;
+      break;
+    case 'palace': noticePrice.min = 10000;
+      break;
+  }
+}
+
 if (apartType && noticePrice) {
-  apartType.addEventListener('change', function () {
-    switch (apartType.value) {
-      case 'appartment': noticePrice.min = 1000;
-        break;
-      case 'hovel': noticePrice.min = 0;
-        break;
-      case 'palace': noticePrice.min = 10000;
-        break;
-    }
-  });
+  apartType.addEventListener('change', switchPrice);
 }
 
 var roomNumber = document.querySelector('#room_number');
 var roomCapacity = document.querySelector('#capacity');
-
+var oneRoom = roomNumber.value === '1';
 if (roomNumber && roomCapacity) {
-  if (roomNumber.value === '1') {
+  if (oneRoom) {
     roomCapacity.value = 0;
   }
-
-  roomNumber.addEventListener('change', function () {
-    roomCapacity.value = (roomNumber.value === '1') ? 0 : 3;
-  });
 }
+
+function switchRoomValue() {
+  roomCapacity.value = (oneRoom) ? 0 : 3;
+}
+
+roomNumber.addEventListener('change', switchRoomValue);
+
