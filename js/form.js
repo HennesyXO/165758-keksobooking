f2b7b73fe04225bed97354097d783aa20d5b9075
Irina <irstrashuk@gmail.com>
2017-02-13 21:32:30 +0200

    made order in code

--- js/form.js
+++ js/form.js
@@ -1,82 +1,36 @@
 'use strict';
+
 // 1. Показ карточки объявления
-var pin = document.querySelectorAll('.pin');
+var pins = document.querySelectorAll('.pin');
 var dialog = document.querySelector('.dialog');
 var dialogClose = dialog.querySelector('.dialog__close');
 var tokioMap = document.querySelector('.tokyo__pin-map');
-// удаляем оранжевый ободок у существующих классов
-function removeActivePin() {
-  var actPin = document.querySelector('.pin--active');
-  if (actPin) {
-    actPin.classList.remove('pin--active');
-  }
-}
+var timeIn = document.querySelector('#time');
+var timeOut = document.querySelector('#timeout');
+var roomNumber = document.querySelector('#room_number');
+
+var activePin = null;
 
 var ENTER_KEY_CODE = 13;
 var ESC_KEY_CODE = 27;
 
-// Array.prototype.forEach.call(pin, function (singlePin) {
-//   singlePin.addEventListener('click', function () {
-//     removeActivePin();
-//     singlePin.classList.add('pin--active');
-//     dialog.style.display = 'block';
-//   });
-// });
+tokioMap.addEventListener('click', onOpen);
+dialogClose.addEventListener('click', onClose);
+timeIn.addEventListener('change', onConnectOptions);
+timeOut.addEventListener('change', onConnectOptionsBack);
+roomNumber.addEventListener('change', onSwitchRoomValue);
 
-Array.prototype.forEach.call(pin, function (singlePin) {
-  singlePin.addEventListener('keydown', function (e) {
-    e.keycode === ENTER_KEY_CODE;
-    removeActivePin();
-    singlePin.classList.add('pin--active');
-    dialog.style.display = 'block';
-  });
-});
-
-  tokioMap.addEventListener('click', function(event) {
-    var clickedElement = event.target;
-
-    if (clickedElement.classList.contains('pin')) {
-
-      removeActivePin();
-      for (var i = 0; i < pin.length; i++) {
-        pin[i].classList.add('pin--active');
-      }
-      dialog.style.display = 'block';
-    }
-  });
-
-// Array.prototype.forEach.call(pin, function (singlePin) {
-//   tokioMap.addEventListener('click', function (event) {
-//     var target = event.target;
-
-//     if (!target.classList.contains('pin')) {
-//       return;
-//     }
-//     event.keycode === ENTER_KEY_CODE;
-//     removeActivePin();
-//     singlePin.classList.add('pin--active');
-//     dialog.style.display = 'block';
-//   });
-// });
-
-
-// =================================
-
-// 2. Закрытие карточки объявления
-function closeModal(e) {
-  e.keycode === ESC_KEY_CODE;
-  removeActivePin();
-  dialog.style.display = 'none';
+for (var i = 0; i < pins.length; i++) {
+  pins[i].addEventListener('keydown', onOpenByEnter);
 }
 
-dialogClose.addEventListener('click', closeModal);
-dialogClose.addEventListener('keydown', closeModal);
 // 3. Проверка правильности введенных данных
 // Заголовок объявления
 var noticeTitle = document.querySelector('#title');
 noticeTitle.required = true;
 noticeTitle.minLength = 30;
 noticeTitle.maxLength = 100;
+
 // Цена за ночь
 var noticePrice = document.querySelector('#price');
 noticePrice.required = true;
@@ -86,24 +40,14 @@ noticePrice.max = 1000000;
 // Адрес
 var noticeAddress = document.querySelector('#address');
 noticeAddress.required = true;
-// 4. Автоматическая корректировка полей в форме.
-// Поля «время заезда» и «время выезда» синхронизированы
-var timeIn = document.querySelector('#time');
-var timeOut = document.querySelector('#timeout');
-
-function connectOptions() {
-  timeOut.value = timeIn.value;
-}
-
-function connectOptionsBack() {
-  timeIn.value = timeOut.value;
-}
-
-timeIn.addEventListener('change', connectOptions);
-timeOut.addEventListener('change', connectOptionsBack);
 
 //  «Тип жилья» синхронизировано с минимальной ценой
 var apartType = document.querySelector('#type');
+apartType.addEventListener('change', switchPrice);
+
+var roomCapacity = document.querySelector('#capacity');
+var oneRoom = roomNumber.value === '1';
+roomCapacity.value = 0;
 
 function switchPrice() {
   switch (apartType.value) {
@@ -116,15 +60,68 @@ function switchPrice() {
   }
 }
 
-apartType.addEventListener('change', switchPrice);
-
-var roomNumber = document.querySelector('#room_number');
-var roomCapacity = document.querySelector('#capacity');
-var oneRoom = roomNumber.value === '1';
-roomCapacity.value = 0;
-
-function switchRoomValue() {
-  roomCapacity.value = (oneRoom) ? 0 : 3;
+function activaePin(pin) {
+  pin.classList.add('pin--active');
+  pin.setAttribute('aria-pressed', 'true');
+  activePin = pin;
 }
 
-roomNumber.addEventListener('change', switchRoomValue);
+function deactivatePin() {
+  if (!activePin) {
+    return;
+  }
+
+  activePin.classList.remove('pin--active');
+  activePin.setAttribute('aria-pressed', 'false');
+  activePin = null;
+}
+
+function onOpen(event) {
+  var target = event.target;
+
+  if (target.tagName === 'IMG') {
+    target = target.parentNode;
+  }
+
+  if (!target.classList.contains('pin')) {
+    return;
+  }
+
+  deactivatePin();
+  activaePin(target);
+  dialog.style.display = 'block';
+}
+
+function onOpenByEnter(event) {
+  var target = event.target;
+
+  if (event.keyCode !== ENTER_KEY_CODE) {
+    return;
+  }
+
+  if (!target.classList.contains('pin')) {
+    return;
+  }
+
+  deactivatePin();
+  activaePin(target);
+  dialog.style.display = 'block';
+}
+
+function onClose(event) {
+  event.preventDefault();
+  deactivatePin();
+  dialog.style.display = 'none';
+}
+
+function onConnectOptions() {
+  timeOut.value = timeIn.value;
+}
+
+function onConnectOptionsBack() {
+  timeIn.value = timeOut.value;
+}
+
+function onSwitchRoomValue() {
+  roomCapacity.value = (oneRoom) ? 0 : 3;
+}
