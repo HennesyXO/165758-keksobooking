dba1fd96c543f4d21be3c91af341dacdddf5350a
Irina <irstrashuk@gmail.com>
2017-02-15 20:42:28 +0200

    excess code has removed

--- js/form.js
+++ js/form.js
@@ -3,6 +3,8 @@
 var timeIn = document.querySelector('#time');
 var timeOut = document.querySelector('#timeout');
 var roomNumber = document.querySelector('#room_number');
+var apartType = document.querySelector('#type');
+var roomCapacity = document.querySelector('#capacity');
 
 /** @constant {Array.<string>} */
 var VALUES_TIME_IN_FIELD = ['12', '13', '14'];
@@ -17,10 +19,6 @@ var VALUES_CAPACITY_FIELD = ['0', '3', '3'];
 window.synchronizeFields(timeIn, timeOut, VALUES_TIME_IN_FIELD, VALUES_TIME_OUT_FIELD, 'value');
 window.synchronizeFields(apartType, noticePrice, VALUES_ACCOMODATION_FIELD, VALUES_MIN_PRICE_FIELD, 'min');
 window.synchronizeFields(roomNumber, roomCapacity, VALUES_ROOM_FIELD, VALUES_CAPACITY_FIELD, 'value');
-
-timeIn.addEventListener('change', onConnectOptions);
-timeOut.addEventListener('change', onConnectOptionsBack);
-roomNumber.addEventListener('change', onSwitchRoomValue);
 
 // 3. Проверка правильности введенных данных
 // Заголовок объявления
@@ -38,36 +36,5 @@ noticePrice.max = 1000000;
 // Адрес
 var noticeAddress = document.querySelector('#address');
 noticeAddress.required = true;
-
-//  «Тип жилья» синхронизировано с минимальной ценой
-var apartType = document.querySelector('#type');
-apartType.addEventListener('change', switchPrice);
-
-var roomCapacity = document.querySelector('#capacity');
-var oneRoom = roomNumber.value === '1';
-roomCapacity.value = 0;
-
-function switchPrice() {
-  switch (apartType.value) {
-    case 'appartment': noticePrice.min = 1000;
-      break;
-    case 'hovel': noticePrice.min = 0;
-      break;
-    case 'palace': noticePrice.min = 10000;
-      break;
-  }
-}
-
-function onConnectOptions() {
-  timeOut.value = timeIn.value;
-}
-
-function onConnectOptionsBack() {
-  timeIn.value = timeOut.value;
-}
-
-function onSwitchRoomValue() {
-  roomCapacity.value = (oneRoom) ? 0 : 3;
-}
 
 window.initializePins();
