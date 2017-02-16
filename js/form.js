a4afa236523ab9dcdcdbbc619151291c16395615
Irina <irstrashuk@gmail.com>
2017-02-16 20:58:25 +0200

    synchrinization has corrected

--- js/form.js
+++ js/form.js
@@ -11,16 +11,16 @@ var noticePrice = document.querySelector('#price');
 var VALUES_TIME_IN_FIELD = ['12', '13', '14'];
 var VALUES_TIME_OUT_FIELD = ['12', '13', '14'];
 
-var VALUES_ACCOMODATION_FIELD = ['apartment', 'shack', 'palace'];
+var VALUES_ACCOMODATION_FIELD = ['apartment', 'hovel', 'palace'];
 var VALUES_MIN_PRICE_FIELD = ['1000', '0', '10000'];
 
 var VALUES_ROOM_FIELD = ['1', '2', '100'];
 var VALUES_CAPACITY_FIELD = ['0', '3', '3'];
 
 window.synchronizeFields(timeIn, timeOut, VALUES_TIME_IN_FIELD, VALUES_TIME_OUT_FIELD, 'value');
-window.synchronizeFields(apartType, noticePrice, VALUES_ACCOMODATION_FIELD, VALUES_MIN_PRICE_FIELD, 'min');
+window.synchronizeFields(apartType, noticePrice, VALUES_ACCOMODATION_FIELD, VALUES_MIN_PRICE_FIELD, 'value');
 window.synchronizeFields(roomNumber, roomCapacity, VALUES_ROOM_FIELD, VALUES_CAPACITY_FIELD, 'value');
-
+window.initializePins();
 // 3. Проверка правильности введенных данных
 // Заголовок объявления
 var noticeTitle = document.querySelector('#title');
@@ -37,4 +37,4 @@ noticePrice.max = 1000000;
 var noticeAddress = document.querySelector('#address');
 noticeAddress.required = true;
 
-window.initializePins();
+
