3176ea1c28715bf47fb5086245325ce073ceabd4
Irina <irstrashuk@gmail.com>
2017-02-17 20:41:54 +0200

    incapsulation

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -1,71 +1,73 @@
 'use strict';
 
 window.initializePins = (function () {
-  var pins = document.querySelectorAll('.pin');
-  var dialog = document.querySelector('.dialog');
-  var dialogClose = dialog.querySelector('.dialog__close');
-  var tokioMap = document.querySelector('.tokyo__pin-map');
-  var activePin = null;
-  var ENTER_KEY_CODE = 13;
+  return function () {
+    var pins = document.querySelectorAll('.pin');
+    var dialog = document.querySelector('.dialog');
+    var dialogClose = dialog.querySelector('.dialog__close');
+    var tokioMap = document.querySelector('.tokyo__pin-map');
+    var activePin = null;
+    var ENTER_KEY_CODE = 13;
 
-  tokioMap.addEventListener('click', onOpen);
-  dialogClose.addEventListener('click', onClose);
+    tokioMap.addEventListener('click', onOpen);
+    dialogClose.addEventListener('click', onClose);
 
-  for (var i = 0; i < pins.length; i++) {
-    pins[i].addEventListener('keydown', onOpenByEnter);
-  }
-
-  function activatePin(pin) {
-    pin.classList.add('pin--active');
-    pin.setAttribute('aria-pressed', 'true');
-    activePin = pin;
-  }
-
-  function deactivatePin() {
-    if (!activePin) {
-      return;
+    for (var i = 0; i < pins.length; i++) {
+      pins[i].addEventListener('keydown', onOpenByEnter);
     }
 
-    activePin.classList.remove('pin--active');
-    activePin.setAttribute('aria-pressed', 'false');
-    activePin = null;
-  }
-
-  function onOpen(event) {
-    var target = event.target;
-
-    if (target.tagName === 'IMG') {
-      target = target.parentNode;
+    function activatePin(pin) {
+      pin.classList.add('pin--active');
+      pin.setAttribute('aria-pressed', 'true');
+      activePin = pin;
     }
 
-    if (!target.classList.contains('pin')) {
-      return;
+    function deactivatePin() {
+      if (!activePin) {
+        return;
+      }
+
+      activePin.classList.remove('pin--active');
+      activePin.setAttribute('aria-pressed', 'false');
+      activePin = null;
     }
+  // return
+    function onOpen(event) {
+      var target = event.target;
 
-    deactivatePin();
-    activatePin(target);
-    dialog.style.display = 'block';
-  }
+      if (target.tagName === 'IMG') {
+        target = target.parentNode;
+      }
 
-  function onOpenByEnter(event) {
-    var target = event.target;
+      if (!target.classList.contains('pin')) {
+        return;
+      }
 
-    if (event.keyCode !== ENTER_KEY_CODE) {
-      return;
+      deactivatePin();
+      activatePin(target);
+      dialog.style.display = 'block';
     }
+  // return
+    function onOpenByEnter(event) {
+      var target = event.target;
 
-    if (!target.classList.contains('pin')) {
-      return;
+      if (event.keyCode !== ENTER_KEY_CODE) {
+        return;
+      }
+
+      if (!target.classList.contains('pin')) {
+        return;
+      }
+
+      deactivatePin();
+      activatePin(target);
+      dialog.style.display = 'block';
     }
-
-    deactivatePin();
-    activatePin(target);
-    dialog.style.display = 'block';
-  }
-
-  function onClose(event) {
-    event.preventDefault();
-    deactivatePin();
-    dialog.style.display = 'none';
-  }
+  // return
+    function onClose(event) {
+      event.preventDefault();
+      deactivatePin();
+      dialog.style.display = 'none';
+    }
+  };
 })();
