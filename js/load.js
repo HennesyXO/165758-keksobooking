4e8b0f8bab5ca3afb0c568b6ea44b0dc3fdb95f6
Irina <irstrashuk@gmail.com>
2017-02-23 11:57:59 +0200

    draft

---
+++ js/load.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function load(url, onload) {
+   onload = intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', 'onLoad');
+  onload = intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data
+  xhr.open('GET', 'DATA_URL');
+  xhr.send();
+})();
